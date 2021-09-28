c65e3b6ced048ab5b952d0a79c98d09981149b8e
BobSmekens <46708969+BobSmekens@users.noreply.github.com>
2021-09-28 22:39:58 +0200

    limiter set for global 60/minute

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\WebAuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,47 +20,49 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
+/////////////////////limit from app/providers/routeserviceprovider/////////////
+Route::middleware(['throttle:global'])->group(function () {
+    /////////////////////// import file and reset table//////////////////////
+    Route::get('/import', [ImportController::class, 'importForm']);
+    Route::post('/import', [ImportController::class, 'import']);
+    Route::get('/reset', [ImportController::class, 'resetTable']);
 
-/////////////////////// import file and reset table//////////////////////
-Route::get('/import', [ImportController::class, 'importForm']);
-Route::post('/import', [ImportController::class, 'import']);
-Route::get('/reset', [ImportController::class, 'resetTable']);
-
-Route::get('/index', [BearController::class, 'bearIndex']);
-Route::get('/json', [JsonController::class, 'bearJson']);
-Route::get('/table', [JsonController::class, 'bearTable']);
-Route::get('/sortclosest', [BearController::class, 'sortClosest']);
-Route::get('/sortfurthest', [BearController::class, 'sortFurthest']);
+    Route::get('/index', [BearController::class, 'bearIndex']);
+    Route::get('/json', [JsonController::class, 'bearJson']);
+    Route::get('/table', [JsonController::class, 'bearTable']);
+    Route::get('/sortclosest', [BearController::class, 'sortClosest']);
+    Route::get('/sortfurthest', [BearController::class, 'sortFurthest']);
 
 
-///////////////////////////     CRUD    ///////////////////////////////////
-Route::get('/createbear', [BearController::class, 'storeShow']);
-Route::post('/createbear', [BearController::class, 'store']);
+    ///////////////////////////     CRUD    ///////////////////////////////////
+    Route::get('/createbear', [BearController::class, 'storeShow']);
+    Route::post('/createbear', [BearController::class, 'store']);
 
-Route::get('/updatebear/{id}', [BearController::class, 'showUpdate']);
-Route::post('/updatebear/{id}', [BearController::class, 'update']);
+    Route::get('/updatebear/{id}', [BearController::class, 'showUpdate']);
+    Route::post('/updatebear/{id}', [BearController::class, 'update']);
 
-Route::get('/bear/{id}', [BearController::class, 'show']);
-Route::post('/bear/{id}/update', [BearController::class, 'update']);
-Route::post('/bear/{id}/delete', [BearController::class, 'destroy']);
+    Route::get('/bear/{id}', [BearController::class, 'show']);
+    Route::post('/bear/{id}/update', [BearController::class, 'update']);
+    Route::post('/bear/{id}/delete', [BearController::class, 'destroy']);
 
-//////////////////////////  download json ///////////////////////////
+    //////////////////////////  download json ///////////////////////////
 
-Route::get('/jsonDownload', [JsonController::class, 'downloadJson']);
+    Route::get('/jsonDownload', [JsonController::class, 'downloadJson']);
 
-////////////////////////users routes////////////////////
-Route::get('/', [WebAuthController::class, 'showRegister']);
+    ////////////////////////users routes////////////////////
+    Route::get('/', [WebAuthController::class, 'showRegister']);
 
-Route::get('/registerUser', [WebAuthController::class, 'showRegister']);
-Route::get('/loginUser', [WebAuthController::class, 'showLogin']);
-Route::get('/logoutUser', [WebAuthController::class, 'logout']);
+    Route::get('/registerUser', [WebAuthController::class, 'showRegister']);
+    Route::get('/loginUser', [WebAuthController::class, 'showLogin']);
+    Route::get('/logoutUser', [WebAuthController::class, 'logout']);
 
-Route::post('/registerUser', [WebAuthController::class, 'register']);
-Route::post('/loginUser', [WebAuthController::class, 'login']);
+    Route::post('/registerUser', [WebAuthController::class, 'register']);
+    Route::post('/loginUser', [WebAuthController::class, 'login']);
 
-// tokens
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
+    // tokens
+    Route::post('/tokens/create', function (Request $request) {
+        $token = $request->user()->createToken($request->token_name);
 
-    return ['token' => $token->plainTextToken];
+        return ['token' => $token->plainTextToken];
+    });
 });
